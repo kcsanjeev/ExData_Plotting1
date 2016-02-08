634c74f7491358199d03c532d6c9652170a1cbb0
Sanjeev K C <sanjeev.kc6@gmail.com>
2016-02-07 20:33:51 -0500

    First and final commit

---
+++ plot1.R
@@ -0,0 +1,43 @@
+rm(list = ls()) 
+rootDir <- "./data"
+fileName <- paste0(rootDir,"/household_power_consumption.txt")
+
+downloadAndExtractFile <- function(){
+    
+    if(!file.exists(rootDir)){
+        dir.create(rootDir)
+    }
+    datasetFullPath <- paste0(rootDir,"/exdata-data-household_power_consumption.zip")
+    if(!file.exists(datasetFullPath)){
+        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile = datasetFullPath)
+    }
+
+    
+    if(!file.exists(fileName)){
+        unzip(zipfile = datasetFullPath,exdir= rootDir)
+    }
+}
+
+plotPng <- function(){
+    downloadAndExtractFile()
+    filteredData <- loadAndFormatFile()
+    par(mfrow = c(1,1))
+    hist(filteredData$Global_active_power, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)")
+    dev.copy(png, file = "plot1.png", width = 480, height = 480)
+    dev.off()
+}
+
+
+loadAndFormatFile <- function(){
+    data <- read.table(file = fileName,sep =";")
+    names(data) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3") 
+    filteredData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]    
+    filteredData$Global_active_power <- as.numeric(as.character(filteredData$Global_active_power))
+    filteredData$Global_reactive_power <- as.numeric(as.character(filteredData$Global_reactive_power))
+    filteredData$Voltage <- as.numeric(as.character(filteredData$Voltage))
+    filteredData <- transform(filteredData, Timestamp = strptime(paste(filteredData$Date, filteredData$Time, sep=" "), "%d/%m/%Y %H:%M:%S"))
+    filteredData$Sub_metering_1 <- as.numeric(as.character(filteredData$Sub_metering_1))
+    filteredData$Sub_metering_2 <- as.numeric(as.character(filteredData$Sub_metering_2))
+    filteredData$Sub_metering_3 <- as.numeric(as.character(filteredData$Sub_metering_3))    
+    filteredData
+}
---
+++ plot3.R
@@ -0,0 +1,46 @@
+rm(list = ls()) 
+rootDir <- "./data"
+fileName <- paste0(rootDir,"/household_power_consumption.txt")
+
+downloadAndExtractFile <- function(){
+    
+    if(!file.exists(rootDir)){
+        dir.create(rootDir)
+    }
+    datasetFullPath <- paste0(rootDir,"/exdata-data-household_power_consumption.zip")
+    if(!file.exists(datasetFullPath)){
+        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile = datasetFullPath)
+    }
+
+    
+    if(!file.exists(fileName)){
+        unzip(zipfile = datasetFullPath,exdir= rootDir)
+    }
+}
+
+plotPng <- function(){
+    downloadAndExtractFile()
+    filteredData <- loadAndFormatFile()
+    par(mfrow = c(1,1))
+    plot(filteredData$Timestamp, filteredData$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+    lines(filteredData$Timestamp,filteredData$Sub_metering_2,type = "l", col  =  "red")
+    lines(filteredData$Timestamp,filteredData$Sub_metering_3,type = "l", col  =  "blue")
+    legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1), lwd = 2.5, col = c("black", "red", "blue"))
+    dev.copy(png, file  =  "plot3.png", width  =  480, height  =  480)
+    dev.off()
+}
+
+
+loadAndFormatFile <- function(){
+    data <- read.table(file  =  fileName,sep  = ";")
+    names(data) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3") 
+    filteredData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]    
+    filteredData$Global_active_power <- as.numeric(as.character(filteredData$Global_active_power))
+    filteredData$Global_reactive_power <- as.numeric(as.character(filteredData$Global_reactive_power))
+    filteredData$Voltage <- as.numeric(as.character(filteredData$Voltage))
+    filteredData <- transform(filteredData, Timestamp  =  strptime(paste(filteredData$Date, filteredData$Time, sep = " "), "%d/%m/%Y %H:%M:%S"))
+    filteredData$Sub_metering_1 <- as.numeric(as.character(filteredData$Sub_metering_1))
+    filteredData$Sub_metering_2 <- as.numeric(as.character(filteredData$Sub_metering_2))
+    filteredData$Sub_metering_3 <- as.numeric(as.character(filteredData$Sub_metering_3))
+    filteredData
+}
